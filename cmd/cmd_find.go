3cc04a463898cd8a92d5b1d1efe7a7bcc317cd64
drypa <dr.drypa.k@mail.ru>
2025-03-11 12:54:18 +0300

    iterate though directories code

--- cmd/cmd_find.go
+++ cmd/cmd_find.go
@@ -2,10 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+	"sync"
 )
-
-type runFunc func(cmd *cobra.Command, args []string)
 
 func NewFindDuplicatesCommand() *cobra.Command {
 	var sourceDir string
@@ -14,7 +16,7 @@ func NewFindDuplicatesCommand() *cobra.Command {
 	c := &cobra.Command{
 		Use:   "find",
 		Short: "duplicates finds duplicate files",
-		Run:   run(sourceDir, targetDir),
+		RunE:  run,
 	}
 	c.Flags().StringVarP(&sourceDir, "source-dir", "s", "", "Source Directory")
 	c.Flags().StringVarP(&targetDir, "target-dir", "t", "", "Target Directory")
@@ -23,9 +25,62 @@ func NewFindDuplicatesCommand() *cobra.Command {
 	return c
 }
 
-func run(sourceDir string, targetDir string) runFunc {
-	return func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Source Directory: %s\n", sourceDir)
-		fmt.Printf("Target Directory: %s\n", targetDir)
+func run(cmd *cobra.Command, args []string) error {
+	sourceDir, err := cmd.Flags().GetString("source-dir")
+	if err != nil {
+		return err
+	}
+	targetDir, err := cmd.Flags().GetString("target-dir")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Source Directory: '%s'\n", sourceDir)
+	fmt.Printf("Target Directory: '%s'\n", targetDir)
+	getFiles(sourceDir)
+	return nil
+}
+func getFiles(dir string) {
+	res := make(chan string)
+	errs := make(chan error)
+	semaphore := make(chan struct{}, 5)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	semaphore <- struct{}{}
+	go getFilesFromDirIncludeChildren(dir, res, errs, &wg, semaphore)
+
+	go func(wg *sync.WaitGroup) {
+		wg.Wait()
+		close(res)
+		close(errs)
+	}(&wg)
+
+	for path := range res {
+		fmt.Println("File:", path)
+	}
+
+	for err := range errs {
+		fmt.Println("Error:", err)
+	}
+}
+
+func getFilesFromDirIncludeChildren(dir string, res chan<- string, errs chan<- error, wg *sync.WaitGroup, sem chan struct{}) {
+	defer wg.Done()
+	defer func() { <-sem }()
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		errs <- errors.Wrapf(err, "error reading directory %s", dir)
+		return
+	}
+	for _, entry := range entries {
+		fullPath := filepath.Join(dir, entry.Name())
+		if entry.IsDir() {
+			wg.Add(1)
+			sem <- struct{}{}
+			getFilesFromDirIncludeChildren(fullPath, res, errs, wg, sem)
+
+		} else {
+			res <- fullPath
+		}
 	}
 }
