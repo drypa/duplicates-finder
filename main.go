2ac280a2d0afe7a7f265c789ee8c09a120a32694
drypa <dr.drypa.k@mail.ru>
2026-08-30 19:28:15 +0300

    graceful shutdown

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"context"
 	"duplicates-github.com/drypa/duplicates-finder/cmd"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	command := cmd.NewFindDuplicatesCommand()
-	if err := command.Execute(); err != nil {
+	command.SetContext(ctx)
+	if err := command.ExecuteContext(ctx); err != nil {
 		fmt.Println(err)
 		return
 	}
